8e327b347fe65c26d587cb6c5a83017f9824fd75
German Gasset <gassetgerman@gmail.com>
2025-01-14 10:55:43 -0500

    finished, now testing time

--- turkish_end_game.c
+++ turkish_end_game.c
@@ -23,3 +23,25 @@ size_t	get_r_count(size_t sorted_pos, t_move move, int *is_reverse)
 		rotation_count = sorted_pos;
 	return (rotation_count);
 }
+
+size_t		get_sorted_position(t_crc_nd *head, int nbr)
+{
+	t_crc_nd	*iter;
+	size_t		sort_i;
+	int			just_bigger;
+	int			i;
+
+	sort_i = 0;
+	just_bigger = -2147483648;
+	iter = head;
+	i = 0;
+	while (iter->contains_val && (iter != head || !i))
+	{
+		sort_i += (i - sort_i) * (iter->val > nbr && iter->val < just_bigger);
+		just_bigger += (iter->val - just_bigger)
+			* (iter->val > nbr && iter->val < just_bigger);
+		iter = iter->next;
+		i++;
+	}
+	return (sort_i);	
+}
